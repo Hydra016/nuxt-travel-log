ee26af6b0cd2cd2a45efc3f665029af2b6761d2f
Hydra016 <haidermansoor459@gmail.com>
2025-12-21 11:21:54 +0200

    [ntl-011] feature: added landing page

---
+++ app/components/app/navbar.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <div class="navbar bg-neutral text-neutral-content">
+      <NuxtLink to="/" class="navbar-start btn btn-ghost text-xl">
+        Nuxt Travel Log
+      </NuxtLink>
+      <div class="navbar-end">
+        <a class="btn btn-accent">
+          Sign in with GitHub
+          <Icon name="tabler:brand-github" size="24" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppNavbar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
