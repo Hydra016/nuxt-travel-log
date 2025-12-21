79fd98bb75565dd62a20ccf7411543de0e0b8ca0
Hydra016 <haidermansoor459@gmail.com>
2025-12-21 11:40:32 +0200

    [ntl-012] feature: added dark and light mode

--- app/components/app/navbar.vue
+++ app/components/app/navbar.vue
@@ -9,6 +9,7 @@
         Nuxt Travel Log
       </NuxtLink>
       <div class="navbar-end">
+        <AppThemeToggle class="mr-4" />
         <a class="btn btn-accent">
           Sign in with GitHub
           <Icon name="tabler:brand-github" size="24" />
