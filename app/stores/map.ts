f83d021eb586be7e1b47c14b1929f8b7587fe379
Hydra016 <haidermansoor459@gmail.com>
2025-12-28 23:33:45 +0200

    [ntl-028] fix: fixed dynamic imports issue with maplibre

--- app/stores/map.ts
+++ app/stores/map.ts
@@ -1,49 +1,52 @@
-import { useMap } from "@indoorequal/vue-maplibre-gl";
-import { LngLatBounds } from "maplibre-gl";
-
 import type { MapPoint } from "~/types";
 
 export const useMapStore = defineStore("useMapStore", () => {
   const mapPoints = ref<MapPoint[]>([]);
-  const map = useMap();
   const selectedPoint = ref<MapPoint | null>(null);
-  let bounds: LngLatBounds | null = null;
 
-  effect(() => {
-    const firstPoint = mapPoints.value[0];
-    if (!firstPoint)
-      return;
-    bounds = mapPoints.value.reduce((bounds, point) => {
-      return bounds.extend([point.long, point.lat]);
-    }, new LngLatBounds(
-      [firstPoint.long, firstPoint.lat],
-      [firstPoint.long, firstPoint.lat],
-    ));
+  async function init() {
+    const { useMap } = await import("@indoorequal/vue-maplibre-gl");
+    const { LngLatBounds } = await import("maplibre-gl");
+    const map = useMap();
+    let bounds: any = null;
 
-    map.map?.fitBounds(bounds, {
-      padding: 100,
-    });
-  });
+    effect(() => {
+      const firstPoint = mapPoints.value[0];
+      if (!firstPoint)
+        return;
+      bounds = mapPoints.value.reduce((bounds, point) => {
+        return bounds.extend([point.long, point.lat]);
+      }, new LngLatBounds(
+        [firstPoint.long, firstPoint.lat],
+        [firstPoint.long, firstPoint.lat],
+      ));
 
-  effect(() => {
-    if (selectedPoint.value) {
-      map.map?.flyTo({
-        center: [selectedPoint.value?.long, selectedPoint.value?.lat],
-        zoom: 5,
-        curve: 1,
-        easing(t) {
-          return t;
-        },
-      });
-    }
-    else if (bounds) {
       map.map?.fitBounds(bounds, {
         padding: 100,
       });
-    }
-  });
+    });
+
+    effect(() => {
+      if (selectedPoint.value) {
+        map.map?.flyTo({
+          center: [selectedPoint.value?.long, selectedPoint.value?.lat],
+          zoom: 5,
+          curve: 1,
+          easing(t) {
+            return t;
+          },
+        });
+      }
+      else if (bounds) {
+        map.map?.fitBounds(bounds, {
+          padding: 100,
+        });
+      }
+    });
+  }
 
   return {
+    init,
     mapPoints,
     selectedPoint,
   };
